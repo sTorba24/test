3a6bf826a70cba3ccbb592036397aa0a6a63dd51
Sviatoslav <55200530+sTorba24@users.noreply.github.com>
2020-12-01 23:58:38 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root = Tk()
+c = Canvas(root, width=540, height=960, bg='white')
+c.pack()
+data = []
+with open('DS7.txt') as f:
+    for line in f:
+        data.append([int(x) for x in line.split()])
+
+for i in range(len(data)):
+    c.create_oval(data[i][0],data[i][1], data[i][0], data[i][1], fill='black')
+
+root.mainloop()
