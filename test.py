c22d5483be414156ec74fb19a0bdfab5396cf129
Sviatoslav <55200530+sTorba24@users.noreply.github.com>
2020-12-02 00:13:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ with open('DS7.txt') as f:
         data.append([int(x) for x in line.split()])
 
 for i in range(len(data)):
-    c.create_oval(data[i][0],data[i][1], data[i][0], data[i][1], fill='black')
+    image = c.create_oval(data[i][0],data[i][1], data[i][0], data[i][1], fill='black')
 
 root.mainloop()
+image.save('res.png')
